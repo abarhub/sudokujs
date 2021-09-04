f2463f038461ff8aaf6b5e6fbb139890b3745cc0
abarhub <abarhub@gmail.com>
2021-09-04 15:18:15 +0200

    passage à angular 11.2.14

--- src/app/component/selection-chiffres/selection-chiffres.component.spec.ts
+++ src/app/component/selection-chiffres/selection-chiffres.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SelectionChiffresComponent } from './selection-chiffres.component';
 
@@ -6,7 +6,7 @@ describe('SelectionChiffresComponent', () => {
   let component: SelectionChiffresComponent;
   let fixture: ComponentFixture<SelectionChiffresComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SelectionChiffresComponent ]
     })
